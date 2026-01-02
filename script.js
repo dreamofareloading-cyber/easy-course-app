bf21c7b226f101c9d3ce014a4b3c46094ec518ab
dreamofareloading-cyber <dreamofareloading@gmail.com>
2026-01-02 03:46:03 -0800

    update js

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ function login() {
   var p = document.getElementById("pass").value;
 
   if (u === "admin" && p === "123") {
-    alert("Admin Login Successful");
+    window.location.href = "admin.html";
+  } else {
+    alert("Login Successful");
+  }
+}
+ssssfunction showLogin() {
+  document.getElementById("loginBox").style.display = "block";
+}
+
+function login() {
+  var u = document.getElementById("user").value;
+  var p = document.getElementById("pass").value;
+
+  if (u === "admin" && p === "123") {
+    window.location.href = "admin.html";
   } else {
     alert("Login Successful");
   }
